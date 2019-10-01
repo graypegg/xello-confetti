edda29444659c593319b626d09de182d8909a32e
Graham Pegg <toish@toi.sh>
2019-10-01 11:44:40 -0400

    Hiding the animation when reduce motion is on

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ class XelloConfetti extends HTMLElement {
     this.mountRoot = this.attachShadow({ mode: 'open' })
     this.scene = new ConfettiScene()
     this.scene.mount(this.mountRoot)
+    const stylesheet = document.createElement('style')
+    stylesheet.innerText = `
+      @media (prefers-reduced-motion: reduce) {
+        canvas {
+          display: none;
+        }
+      }
+    `
+    this.mountRoot.appendChild(stylesheet)
   }
 
   disconnectedCallback () {
